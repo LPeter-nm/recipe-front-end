5e75ed089f80fc62e1ec5641046b3955fe36b700
Jhoão <jhpdrnsantos2@gmail.com>
2025-01-31 09:30:30 -0300

    feat: alterado protocolo de update recipe

--- src/components/card_recipe/server_card_update_recipe.tsx
+++ src/components/card_recipe/server_card_update_recipe.tsx
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 export const fetchUpdateRecipe = async (id: string) => {
   const jwt = (await cookies()).get("JWT");
   console.log(jwt!.value)
-  const response = await api.patch(`recipe/favorite/${id}`, {}, {
+  const response = await api.put(`recipe/favorite/${id}`, {}, {
     headers: {
       authorization: `Bearer ${jwt!.value}`,
     },
